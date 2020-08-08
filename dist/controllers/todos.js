acd695a4575aa6dd56a696052eba382d012490db
Tigran Asriyan <tigran.asriyan@gmail.com>
2020-08-07 23:10:23 -0400

    finished project

---
+++ dist/controllers/todos.js
@@ -0,0 +1,33 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.deleteTodos = exports.updateTodo = exports.getTodos = exports.createTodo = void 0;
+const todo_1 = require("../models/todo");
+const TODOS = [];
+exports.createTodo = (req, res, next) => {
+    const text = req.body.text;
+    const newTodo = new todo_1.Todo(Math.random().toString(), text);
+    TODOS.push(newTodo);
+    res.status(201).json({ message: "Created teh dodo", createdTodo: newTodo });
+};
+exports.getTodos = (req, res, next) => {
+    res.status(200).json({ todos: TODOS });
+};
+exports.updateTodo = (req, res, next) => {
+    const todoId = req.params.id;
+    const updatedText = req.body.text;
+    const todoIndex = TODOS.findIndex((todo) => todo.id === todoId);
+    if (todoIndex < 0) {
+        throw new Error("Could not find todo!");
+    }
+    TODOS[todoIndex] = new todo_1.Todo(TODOS[todoIndex].id, updatedText);
+    res.status(200).json({ message: "Updated!", todos: TODOS[todoIndex] });
+};
+exports.deleteTodos = (req, res, next) => {
+    const todoId = req.params.id;
+    const todoIndex = TODOS.findIndex((todo) => todo.id === todoId);
+    if (todoIndex < 0) {
+        throw new Error("Could not find todo!");
+    }
+    TODOS.splice(todoIndex, 1);
+    res.status(200).json({ message: "Todo deleted!" });
+};
